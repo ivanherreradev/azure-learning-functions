0c65ab433e259b482deaff16d8a53e02db9d4fac
ivanherreradev <ivanalejandroherrera00@gmail.com>
2026-06-13 18:13:13 -0300

    updates

--- src/functions/func-learning.ts
+++ src/functions/func-learning.ts
@@ -1,11 +1,17 @@
-import { app, InvocationContext } from "@azure/functions";
+import { app, InvocationContext, output } from "@azure/functions";
 
 export async function funcLearning(queueItem: unknown, context: InvocationContext): Promise<void> {
     context.log('Storage queue function processed work item:', queueItem);
+    context.log(context)
+    const outputMessage = `Mensaje procesado ${queueItem}`
+    context.extraOutputs.set(outputQueue, outputMessage)
 }
+
+const outputQueue = output.storageQueue({queueName:'output-queue', connection:'AzureWebJobsStorage'})
 
 app.storageQueue('func-learning', {
     queueName: 'js-queue-items',
     connection: 'AzureWebJobsStorage',
+    extraOutputs: [outputQueue],
     handler: funcLearning
 });
